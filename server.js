b42631d098d038d62f93aa6453df6fbc1f5babfe
Emily Resch <eresch527@gmail.com>
2020-05-04 10:59:47 -0500

    config and server started

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 8090;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/books_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() { console.log(PORT); });
