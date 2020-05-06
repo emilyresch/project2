18c5a22b1fc7a538839f865ede7763e9274481ef
Rensy <rensy.aikara@gmail.com>
2020-05-06 00:05:36 -0500

    added api call and response details to the router and handlebar

--- public/assets/js/books.js
+++ public/assets/js/books.js
@@ -1,42 +1,25 @@
-// $(document).function() {
-    //search button id
-    var searchBooks = $("#");
+
+    var searchBooks = $("#searchButton");
 
 
     searchBooks.on("click",function(event){
         event.preventDefault();
-        //book name id
-        var bookName = $("#").val();
-        apicallFunction(bookName);
-        })
-  
-    //book name id
-    $("#").keypress(function(event){
-        var keyCode = (event.keyCode ? event.keyCode : event.which);
-        if(keyCode == '13'){
-            // book name id
-            var bookName = $("#").val();
-        apicallFunction(bookName);
+        var bookName = $("#bookSearch").val();
+        if(!bookName){
+            return;
         }
+        searchforBook(bookName);
+        bookName.val("");
     })
-
-    function apicallFunction(bookName) {
-        var nameArray = bookName.split(" ");
-
-        var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + bookName;
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-            var date = today.getMonth() + '/' + (today.getDate()) + '/' + today.getFullYear();
-            console.log(date);
-            $("#cityName").text(cityName);
-            $("#cityDate").text(date);
-
-            var tempC = response.main.temp;
-
-        });
+    function searchforBook(bookName){
+        $.post("/api/booksearch/bookname",{
+            title: bookName
+        })
+        .then(function(){
+            console.log("Searching for book");
+            location.reload();
+        })
     }
-// })
+
+
+
