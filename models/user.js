3cb6f1eac16ee17e7235d98cbade1eb92d2b8859
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-06 11:08:27 -0500

    Login works. Needs to redirect to the books page if authenticated.

--- models/user.js
+++ models/user.js
@@ -28,13 +28,13 @@ module.exports = function (sequelize, DataTypes) {
         });
     };
 
-    User.prototype.validPassword = function(password) {
+    User.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
-      };
+    };
 
-      User.addHook("beforeCreate", function(user) {
+    User.addHook("beforeCreate", function (user) {
         user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-      });
+    });
 
     return User;
 }
