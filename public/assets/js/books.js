ddfdf92973a9c0b001cabbba64e432a01bb1c204
Rensy <rensy.aikara@gmail.com>
2020-05-07 17:10:24 -0500

    highlighting buttons work

--- public/assets/js/books.js
+++ public/assets/js/books.js
@@ -107,8 +107,8 @@
 //click star to add to wishlist (Database)
 $(document).on("click", ".wishlist-btn", function (event) {
     event.preventDefault();
-    console.log("wish");
-    
+    // console.log("wish");
+    $(this).text("star");
 
     var newBook = {
         author: $(this).data("author"),
@@ -130,7 +130,7 @@ $(document).on("click", ".wishlist-btn", function (event) {
 //click tag to add to hav_read (Database)
 $(document).on("click",".read-btn", function (event) {
     event.preventDefault();
-
+    $(this).text("bookmark");
     var newBook = {
         author: $(this).data("author"),
         title: $(this).data("title"),
